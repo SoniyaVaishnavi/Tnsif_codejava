3ea6bb601e19d69e411741baad9eea54a33302c6
Soniya Vaishnavi <Soniya Vaishnavi@VAISHU>
2025-10-23 19:25:21 +0530

    Commit

---
+++ Tnsif_78/src/com/tnsif_78/Interfacedemo.java
@@ -0,0 +1,29 @@
+package com.tnsif_78;
+	
+	interface Shape{
+		 
+		 //declaration
+	    void draw();
+	}
+
+	public class Interfacedemo implements Shape{
+		
+		//implementation or explanation
+		public void draw() {
+			System.out.println("Drawing a circle in an interface");
+		}
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			Interfacedemo id = new Interfacedemo();
+			id.draw();
+			
+
+			
+		}
+
+	}
+
+
+
